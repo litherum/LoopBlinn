8ebde2acc5b122b205143ac352a9617ada8c3e9c
Myles C. Maxfield <mmaxfield@apple.com>
2015-02-09 18:32:20 -0500

    More debugging

--- LoopBlinn/Fragment.fs
+++ LoopBlinn/Fragment.fs
@@ -6,11 +6,11 @@ out vec4 outColor;
 
 void main() {
     //if ((coordinateV.x * coordinateV.x - coordinateV.y < 0) ^^ bool(orientationV))
-    float k = coordinateV.x;
+    /*float k = coordinateV.x;
     float l = coordinateV.y;
     float m = coordinateV.z;
-    if (k * k * k - l * m < 0)
+    if (k * k * k - l * m < 0)*/
         outColor = vec4(1.0, 0.0, 0.0, 1.0);
-    else
-        outColor = vec4(0.0, 0.0, 0.0, 1.0);
+    //else
+    //    outColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
