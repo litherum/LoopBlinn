e48c66d7742bc75a3035d83c8af99efe382cc67b
Litherum <litherum@gmail.com>
2015-02-14 11:34:22 -0800

    Curve antialiasing

--- LoopBlinn/Fragment.fs
+++ LoopBlinn/Fragment.fs
@@ -5,12 +5,11 @@ in vec3 coordinateV;
 out vec4 outColor;
 
 void main() {
-    //if ((coordinateV.x * coordinateV.x - coordinateV.y < 0) ^^ bool(orientationV))
     float k = coordinateV.x;
     float l = coordinateV.y;
     float m = coordinateV.z;
-    if (k * k * k - l * m < 0)
-        outColor = vec4(1.0, 0.0, 0.0, 1.0);
-    else
-        outColor = vec4(0.0, 0.0, 0.0, 1.0);
+    float f = k * k * k - l * m;
+    vec2 grad = vec2(dFdx(f), dFdy(f));
+    float signedDistance = f / length(grad);
+    outColor = vec4(smoothstep(0.5, -0.5, signedDistance), 0.0, 0.0, 1.0);
 }
